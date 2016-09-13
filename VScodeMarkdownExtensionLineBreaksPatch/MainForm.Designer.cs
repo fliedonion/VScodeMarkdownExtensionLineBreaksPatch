fc664bf64dbc0ad87cd32c68bd644038a54cb204
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2016-09-14 02:33:06 +0900

    version 1.0.0
    for vscode-markdown 0.2.0

---
+++ VScodeMarkdownExtensionLineBreaksPatch/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VScodeMarkdownExtensionLineBreaksPatch {
+    partial class MainForm {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.execButton = new System.Windows.Forms.Button();
+            this.vscodePathTextBox = new System.Windows.Forms.TextBox();
+            this.logText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // execButton
+            // 
+            this.execButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.execButton.Location = new System.Drawing.Point(762, 80);
+            this.execButton.Name = "execButton";
+            this.execButton.Size = new System.Drawing.Size(151, 32);
+            this.execButton.TabIndex = 0;
+            this.execButton.Text = "Run";
+            this.execButton.UseVisualStyleBackColor = true;
+            this.execButton.Click += new System.EventHandler(this.execButton_Click);
+            // 
+            // vscodePathTextBox
+            // 
+            this.vscodePathTextBox.Location = new System.Drawing.Point(51, 49);
+            this.vscodePathTextBox.Name = "vscodePathTextBox";
+            this.vscodePathTextBox.Size = new System.Drawing.Size(761, 25);
+            this.vscodePathTextBox.TabIndex = 1;
+            // 
+            // logText
+            // 
+            this.logText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logText.Location = new System.Drawing.Point(51, 118);
+            this.logText.Multiline = true;
+            this.logText.Name = "logText";
+            this.logText.ReadOnly = true;
+            this.logText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logText.Size = new System.Drawing.Size(862, 181);
+            this.logText.TabIndex = 2;
+            this.logText.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 72);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "after this patch success, you can specify markdown-it\'s `breaks` \r\noption in sett" +
+    "ings like following:\r\n\r\n    \"markdown.markdown-it-breaks\": true";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Found Path:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 426);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logText);
+            this.Controls.Add(this.vscodePathTextBox);
+            this.Controls.Add(this.execButton);
+            this.Name = "MainForm";
+            this.Text = "vscode-markdown patch";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execButton;
+        private System.Windows.Forms.TextBox vscodePathTextBox;
+        private System.Windows.Forms.TextBox logText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ VScodeMarkdownExtensionLineBreaksPatch/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VScodeMarkdownExtensionLineBreaksPatch {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+            vscodePathTextBox.ReadOnly = true;
+        }
+
+        private const string vscode = @"C:\Program Files (x86)\Microsoft VS Code";
+        private const string target = @"C:\Program Files (x86)\Microsoft VS Code\resources\app\extensions\markdown\out\extension.js";
+        private const string package_json = @"C:\Program Files (x86)\Microsoft VS Code\resources\app\extensions\markdown\package.json";
+
+        private Dictionary<string, string> supportedVersions = new Dictionary<string, string>{
+                                                     {@"""version"": ""0.2.0""", "0.2.0"}
+                                                 };
+
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            if(Directory.Exists(vscode) && File.Exists(target)){
+                vscodePathTextBox.Text = target;
+            }
+            else {
+                MessageBox.Show("VScode not found.", "sorry", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Close();
+            }
+        }
+
+        private void AppendLog(string text) {
+            logText.AppendText(text + Environment.NewLine);
+        }
+
+
+        private void execButton_Click(object sender, EventArgs e) {
+            execButton.Visible = false;
+            try {
+                AppendLog("Start :" + DateTime.Now.ToString());
+
+                AppendLog("Checking  is target file expected version.");
+
+                if (!CheckVersion()) {
+                    if(DialogResult.Yes != MessageBox.Show("vscode-markdown may not supported version. Do you want to continue ?", "confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)){
+
+                        AppendLog("User canceled.");
+                        return;
+                    }
+                }
+                AppendLog("Affect Patch.");
+
+                var result = RunPatch();
+
+
+                AppendLog("Display Result.");
+                switch (result) {
+                    case Results.Success:
+                        MessageBox.Show("Complete.", "vscode-markdown patch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    case Results.MayBeAlreadyPatched:
+                        MessageBox.Show("Targetfile may be already patched.", "vscode-markdown patch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    case Results.TargetLineNotFound:
+                        MessageBox.Show("Target line couldn't find. Nothing to do.", "vscode-markdown patch", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        break;
+                    default:
+                        MessageBox.Show("Unknown Result code("+result.ToString()+").", "vscode-markdown patch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error." + Environment.NewLine + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally {
+                execButton.Visible = true; AppendLog("End :" + DateTime.Now.ToString());
+            }
+        }
+
+        private bool CheckVersion() {
+            using (var sr = new StreamReader(package_json)) {
+                var line = "";
+                while ((line = sr.ReadLine()) != null) {
+                    if(supportedVersions.Keys.Any(x => line.Contains(x))){
+                        // var version = supportedVersions.First(x => line.Contains(x.Key)).Value;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public enum Results{
+            None,
+            Success,
+            TargetLineNotFound,
+            MayBeAlreadyPatched,
+        }
+
+        private Results RunPatch() {
+            var lineset = GetLineSet();
+
+            var inserted = false;
+            var tmp = Path.GetTempFileName();
+            AppendLog("PatchStart.");
+
+            using(var sw = new StreamWriter(tmp, false, Encoding.UTF8))
+            using (var sr = new StreamReader(target, Encoding.UTF8)) {
+                var line = "";
+                var beforeLineFoundIndex = 0;
+
+                while ((line = sr.ReadLine()) != null) {
+                    if (line == lineset.AfterLine && beforeLineFoundIndex == lineset.BeforeLines.Count) {
+                        sw.WriteLine(lineset.Insert);
+                        inserted = true;
+                        AppendLog("Data Inserted.");
+                    }
+                    if (line == lineset.Insert && beforeLineFoundIndex == lineset.BeforeLines.Count) {
+                        AppendLog("Detect `Already patched`.");
+                        return Results.MayBeAlreadyPatched;
+                    }
+                    if (!inserted) {
+                        if (beforeLineFoundIndex < lineset.BeforeLines.Count && line == lineset.BeforeLines[beforeLineFoundIndex]) {
+                            beforeLineFoundIndex++;
+                        }
+                        else {
+                            beforeLineFoundIndex = 0;
+                        }
+                    }
+                    sw.WriteLine(line);
+                }
+            }
+            if (!inserted) {
+                AppendLog("Target line missing.");
+                return Results.TargetLineNotFound;
+            }
+
+            AppendLog("Backup original file.");
+            File.Move(target, target + DateTime.Now.ToString(".yyyyMMddHHmmss") + ".backup");
+
+            AppendLog("Copy patched file to vscode-markdown directory(" + target + ").");
+            File.Copy(tmp, target);
+
+            return Results.Success;
+        }
+
+        class LineSet{
+            public List<string> BeforeLines ;
+            public string Insert = "            breaks: vscode.workspace.getConfiguration('markdown')['markdown-it-breaks']||false,";
+            public string AfterLine;
+            
+        }
+
+        private LineSet GetLineSet(){
+            var ls = new LineSet();
+            ls.BeforeLines = new List<string> {
+                                    "    MDDocumentContentProvider.prototype.createRenderer = function () {"
+                                    ,"        var hljs = require('highlight.js');"
+                                    ,"        var mdnh = require('markdown-it-named-headers');"
+                                    ,"        var md = require('markdown-it')({"};
+            ls.AfterLine = "            html: true,";
+
+            return ls;
+
+    }
+
+
+
+    }
+}
